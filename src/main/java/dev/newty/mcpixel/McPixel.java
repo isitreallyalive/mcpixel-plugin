02caeada75dcc85428da130707e4e92e323df14f
newt <hi@newty.dev>
2026-04-06 13:30:22 +0100

    feat: initial commit

---
+++ src/main/java/dev/newty/mcpixel/McPixel.java
@@ -0,0 +1,16 @@
+package dev.newty.mcpixel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class McPixel extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
